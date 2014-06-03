886200507239563981d8f2d2a9656fa5d118eba1
135yshr <isago@praisefirst.co.jp>
2014-06-04 07:22:19 +0900

    Initialize commit.

---
+++ Twift/TimelineViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TimelineViewController.swift
+//  Twift
+//
+//  Created by 135yshr on 2014/06/04.
+//  Copyright (c) 2014年 135yshr. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TimelineViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pressComposeButton(sender : UIButton) {
+        if(TWTweetComposeViewController.canSendTweet()) {
+            var composeViewController = TWTweetComposeViewController()
+            self.presentModalViewController(composeViewController, animated: true)
+        }
+    }
+    
+}
+
